ddf8ef2d89c719d6f56ed2e92885d8a7f242c558
Wasim Raja <wasim7raja10@gmail.com>
2021-09-04 23:39:23 +0530

    fetched list API and Random dog API

--- js/app.js
+++ js/app.js
@@ -1,18 +1,40 @@
 const urlBreedList = 'https://dog.ceo/api/breeds/list/all';
-let selectedBreed;
-const breed = document.querySelector('#breed');
+let selectedBreed = 'labrador';
+let urlDoggo = `https://dog.ceo/api/breed/${selectedBreed}/images/random`;
+let breedList = document.querySelector('#breed');
+let dogImg = document.querySelector('.img');
 
-function addDog() {
+function addDogList() {
   fetch(urlBreedList)
-  .then(response => response.json())
-  .then(data => {
-    const list = Object.keys(data.message);
-    list.forEach( nameOfBreed => {
-      const option = document.createElement('OPTION')
-      option.innerText = nameOfBreed;
-      breed.appendChild(option);
-    });
-  })
+    .then(response => response.json())
+    .then(data => {
+      const list = Object.keys(data.message);
+      list.forEach(nameOfBreed => {
+        const option = document.createElement('OPTION')
+        option.innerText = nameOfBreed;
+        option.value = nameOfBreed;
+        breedList.appendChild(option);
+      });
+    })
 }
 
-addDog();
+const getImg = function getImg(urlDoggo) {
+  fetch(urlDoggo)
+    .then(response => response.json())
+    .then(data => {
+      dogImg.src = data.message;
+      dogImg.alt = `${selectedBreed} dog picture`;
+    })
+}
+
+function showImg() {
+  breedList.addEventListener('change', (e) => {
+    selectedBreed = (e.target.value);
+    urlDoggo = `https://dog.ceo/api/breed/${selectedBreed}/images/random`;
+    getImg(urlDoggo);
+  });
+}
+
+addDogList();
+getImg(urlDoggo);
+showImg();
